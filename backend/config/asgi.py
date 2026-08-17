103a2a0a6709ee1f31aae1a83f9b0364a26365aa
Saeed <saeedparsi2002@gmail.com>
2026-08-17 19:47:21 +0330

    fix: serve static files in ASGI development

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -3,16 +3,21 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base")
 
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.conf import settings
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
 from django.core.asgi import get_asgi_application
 
 django_asgi_app = get_asgi_application()
+http_application = (
+    ASGIStaticFilesHandler(django_asgi_app) if settings.DEBUG else django_asgi_app
+)
 
 from apps.chat.realtime import JwtAuthMiddleware
 from apps.chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
+        "http": http_application,
         "websocket": JwtAuthMiddleware(URLRouter(websocket_urlpatterns)),
     }
 )
